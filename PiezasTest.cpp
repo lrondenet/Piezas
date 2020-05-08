d3742eed00b52c88a25e445786d020cf6234f34c
lrondenet <lrondenet1@mail.csuchico.edu>
2020-05-08 03:31:15 -0700

    adding more tests

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -23,27 +23,60 @@ TEST(PiezasTest, sanityCheck)
 
 TEST(PiezasTest, drop_piece_check)
 {
-
 	Piezas obj;
-	int ret = obj.dropPiece(2);
-	cout << ret << endl;
+	obj.dropPiece(0);
+	obj.dropPiece(1);
+	obj.dropPiece(2);
+	obj.dropPiece(3);
+	obj.dropPiece(0);
+	obj.dropPiece(1);
+	obj.dropPiece(2);
+	obj.dropPiece(3);
+	obj.dropPiece(0);
+	obj.dropPiece(1);
+	obj.dropPiece(2);
+	obj.dropPiece(3);
 
 }
 
 TEST(PiezasTest, greaterthan_num)
 {
-
 	Piezas obj;
 	obj.dropPiece(4);
-
 }
 
 TEST(PiezasTest, neg_num)
 {
-
 	Piezas obj;
-	obj.dropPiece(4);
-
+	obj.dropPiece(-1);
 }
+
+TEST(PiezasTest, out_of_bounds1)
+{
+	Piezas obj;
+	obj.pieceAt(3,4);
+}
+
+TEST(PiezasTest, out_of_bounds2)
+{
+	Piezas obj;
+	obj.pieceAt(-1,-2);
+}
+
+
+TEST(PiezasTest, pieceAt_checkX)
+{
+	Piezas obj;
+	obj.pieceAt(2,0);
+}
+
+TEST(PiezasTest, pieceAt_checkO)
+{
+	Piezas obj;
+	obj.pieceAt(2,1);
+}
+
+
+
 
 
