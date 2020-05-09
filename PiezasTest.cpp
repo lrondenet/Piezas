df892d2d231784eb9d93654cd2c70ed33e4436fb
lrondenet <lrondenet1@mail.csuchico.edu>
2020-05-08 23:18:22 -0700

    fixing changes

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -228,12 +228,13 @@ TEST(PiezasTest, gameState_five)
 	obj.dropPiece(2);
 	obj.dropPiece(3);
 	obj.dropPiece(0);
-	obj.dropPiece(3);
-	obj.dropPiece(2);
-	obj.dropPiece(2);
+	obj.dropPiece(0);
 	obj.dropPiece(1);
+	obj.dropPiece(1);
+	obj.dropPiece(2);
 	obj.dropPiece(3);
 	obj.dropPiece(2);
+	obj.dropPiece(0);
 	obj.dropPiece(3);
 	obj.gameState();
 
