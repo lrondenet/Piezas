7251249803c23982925ea7ed5fd7084de0f7c7d3
lrondenet <lrondenet1@mail.csuchico.edu>
2020-05-08 22:16:47 -0700

    updating functions

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -209,7 +209,7 @@ TEST(PiezasTest, gameState_four)
 	obj.dropPiece(0);
 	obj.dropPiece(1);
 	obj.dropPiece(1);
-	// obj.dropPiece(2);
+	obj.dropPiece(2);
 	// obj.dropPiece(1);
 	// obj.dropPiece(2);
 	// obj.dropPiece(1);
