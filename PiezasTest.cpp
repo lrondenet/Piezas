77237606872969827657919398dfd3e556583204
lrondenet <lrondenet1@mail.csuchico.edu>
2020-05-08 20:41:07 -0700

    adding functions

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -149,14 +149,14 @@ TEST(PiezasTest, gameState_one)
 	obj.dropPiece(0);
 	obj.dropPiece(0);
 	obj.dropPiece(1);
-	obj.dropPiece(2);
-	obj.dropPiece(3);
-	obj.dropPiece(0);
 	obj.dropPiece(1);
-	obj.dropPiece(2);
-	obj.dropPiece(3);
-	obj.dropPiece(2);
-	obj.dropPiece(3);
+	// obj.dropPiece(3);
+	// obj.dropPiece(0);
+	// obj.dropPiece(1);
+	// obj.dropPiece(1);
+	// obj.dropPiece(3);
+	// obj.dropPiece(2);
+	// obj.dropPiece(3);
 	obj.gameState();
 }
 
